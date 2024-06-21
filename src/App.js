337fff8f73d61b0b69bec232e2f54ef051389ad3
meghaverma009 <meghaverma1884@gmail.com>
2024-06-21 16:32:51 +0530

    Add New File

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+ import TextForm from "./components/TextForm";
+//  import About from "./components/About"
+ import React, {useState} from "react";
+ import Alert from "./components/Alert";
+//  import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
+
+function App() {
+  const[mode,setMode] = useState('light') //whether dark mode is enabled or not
+  const[alert,setAlert] = useState(null)
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg: message,
+      type:type,
+    })
+    setTimeout(() =>{
+      setAlert(null);
+    },1500);
+  
+  }
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark'); //setmode is function and calling here
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'TextUtils - Dark Mode'; 
+    //   setInterval(() =>{
+    //     document.title = 'TextUtils is Amazing Mode';
+    //   }, 2000);
+    //   setInterval(() =>{
+    //   document.title = 'Install TextUtils Now'; 
+    //   }, 1500);
+   }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'White';
+      showAlert("Light mode has been enabled","success");
+      document.title = 'TextUtils - Light Mode'; 
+    }
+  }
+  return (
+    <>
+      {/* <Navbar title="React Starting" aboutText= "About React" />  */}
+      {/* <Router> */}
+      <Navbar title="TextUtils" aboutText="About"  mode={mode} toggleMode={toggleMode}/>
+      <Alert alert = {alert}/>
+      <div className="container my-3">
+       {/* <Switch> */}
+          {/* <Route exact path="/about">
+            <About />
+          </Route>  */}
+{/*       
+           <Route exact path="/">  */}
+          <TextForm showAlert = {showAlert} heading="Enter the text to analyze below" mode = {mode}/>  
+           {/* </Route> */}
+      {/* </Switch>  */}
+        </div>
+      {/* </Router>  */}
+    </>
+  );
+}
+
+export default App;
+//npm uninstall react-router-dom
+//npm install react-router-dom@5.2.0
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+const a = "Harry";
+const b = "Rohan";
+const c = "Priyanka";
+const d = "Sahil";
+
+export default c;
